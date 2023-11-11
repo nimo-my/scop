195fe576bcb8b761298993f247a154703dd9b6dd
jisookim <nimmo9040@gmail.com>
2023-11-11 18:21:42 +0900

    normalize

--- src/parse.cpp
+++ src/parse.cpp
@@ -2,9 +2,9 @@
 
 // https://www.cs.cmu.edu/~mbz/personal/graphics/obj.html
 
-Parse::Parse()
-: mtlInfo(""), typePrefix(""), vertexNormalIdx(0), vertexTexIdx(0), rangeLimit(0)
-{}
+Parse::Parse() : mtlInfo(""), typePrefix(""), vertexNormalIdx(0), vertexTexIdx(0), rangeLimit(0)
+{
+}
 
 void Parse::Parser(std::string filename)
 {
@@ -27,10 +27,13 @@ void Parse::Parser(std::string filename)
         {
             float vert1 = 0.0f, vert2 = 0.0f, vert3 = 0.0f;
             ss >> vert1 >> vert2 >> vert3;
-            vertexPosition.push_back(vert1);
-            vertexPosition.push_back(vert2);
-            vertexPosition.push_back(vert3);
+            std::vector<float> buffer;
+
+            buffer.push_back(vert1);
+            buffer.push_back(vert2);
+            buffer.push_back(vert3);
             rangeLimit++;
+            vertexPosition.push_back(buffer);
         }
         else if (typePrefix == "f") // vertex face information
         {
@@ -38,11 +41,10 @@ void Parse::Parser(std::string filename)
             std::vector<size_t> vertexBuffer;
             while (ss >> vertexData)
             {
-                std::cout << vertexData << std::endl;
                 std::istringstream vertexIdxInfoStream(vertexData);
                 float vertexIdx, textureIdx, normalIdx;
 
-                vertexIdxInfoStream >> vertexIdx; // 공백을 기준으로 문자열을 parsing
+                vertexIdxInfoStream >> vertexIdx;      // 공백을 기준으로 문자열을 parsing
                 vertexBuffer.push_back(vertexIdx - 1); // while문 돌면서 f 뒤에 오는 인자 하나씩 넣어주기
                 // if (vertexIdxInfoStream.peek() == '/') // .peek() : 스트림에서 빼오지는 않고 읽기만 한다
                 // {
@@ -66,7 +68,6 @@ void Parse::Parser(std::string filename)
             {
                 this->faces.push_back(std::vector<size_t>{vertexBuffer[0], vertexBuffer[1], vertexBuffer[2]});
                 this->faces.push_back(std::vector<size_t>{vertexBuffer[0], vertexBuffer[2], vertexBuffer[3]});
-                
             }
             vertexBuffer.clear();
         }
@@ -75,7 +76,7 @@ void Parse::Parser(std::string filename)
         typePrefix = ""; // typePrefix 초기화
     }
     file.close();
-    
+
     std::ifstream file2(filename);
     if (!file2.is_open())
     {
@@ -173,63 +174,73 @@ void Parse::parseMtlFile()
 
 void Parse::makeVBO()
 {
-    normalizing();
+    for (std::vector line : faces)
+    {
+        normalizing(line); // make vertex normal
+    }
     // texture
     // TODO : need to implement code.
 }
 
-void Parse::normalizing()
+void Parse::normalizing(std::vector<size_t> facesLine)
 {
-    // initialize
-    
+    std::vector<float> &v1 = vertexPosition[facesLine[0]];
+    std::vector<float> &v2 = vertexPosition[facesLine[1]];
+    std::vector<float> &v3 = vertexPosition[facesLine[2]];
+
+    glm::vec3 vec1 = {v2[0] - v1[0], v2[1] - v1[1], v2[2] - v1[2]};
+    glm::vec3 vec2 = {v3[0] - v1[0], v3[1] - v1[1], v3[2] - v1[2]};
+
+    glm::vec3 crossed = glm::cross(vec1, vec2);
+    glm::vec3 normal = glm::normalize(crossed);
+
+    std::cout << glm::to_string(normal) << std::endl;
+    // std::vector<size_t> buffer;
+    // buffer[i] = glm::normalize((normal + vertexPosition[i]));
+    // buffer[i + 1] = glm::normalize((normal + vertexPosition[i + 1]));
+    // buffer[i + 2] = glm::normalize((normal + vertexPosition[i + 2]));
+    normals.push_back(normal);
 }
 
 void Parse::printVertexInfo()
 {
-    std::cout << "*** VERTICES **** :: \n";
-    std::vector<float>::iterator iter;
-    std::cout << "1) vertex Position : ";
-    for (iter = vertexPosition.begin(); iter != vertexPosition.end(); ++iter)
-    {
-       std::cout << *iter << " ";
-    }
+    // std::cout << "*** VERTICES **** :: \n";
+    // std::vector<float>::iterator iter;
+    // std::cout << "1) vertex Position : ";
+    // for (iter = vertexPosition.begin(); iter != vertexPosition.end(); ++iter)
+    // {
+    //     std::cout << *iter << " ";
+    // }
     std::cout << "\n2) vertex Normal : ";
     std::vector<float>::iterator iter2;
     for (iter2 = vertexNormal.begin(); iter2 != vertexNormal.end(); ++iter2)
     {
-       std::cout << *iter2 << " ";
+        std::cout << *iter2 << " ";
     }
     std::cout << "\n3) vertex Texture coord : ";
     std::vector<float>::iterator iter3;
     for (iter3 = vertexTexCoord.begin(); iter3 != vertexTexCoord.end(); ++iter3)
     {
-       std::cout << *iter3 << " ";
+        std::cout << *iter3 << " ";
     }
     std::cout << "\n4) vertex idx : ";
     std::vector<float>::iterator iter7;
     for (iter7 = attribute.begin(); iter7 != attribute.end(); ++iter7)
     {
-       std::cout << *iter7 << " ";
-    }
-
-    std::cout << "\n5) faces size: " << faces.size() << "\n";
-    
-    for (size_t i = 0; i < faces.size(); i++)
-    {
-
+        std::cout << *iter7 << " ";
     }
 
     std::cout << "\n8) diffuse idx : ";
     std::vector<float>::iterator iter8;
     for (iter8 = diffuse.begin(); iter8 != diffuse.end(); ++iter8)
     {
-       std::cout << *iter8 << " ";
+        std::cout << *iter8 << " ";
     }
     std::cout << "\n9) specular idx : ";
     std::vector<float>::iterator iter9;
     for (iter9 = specular.begin(); iter9 != specular.end(); ++iter9)
     {
-       std::cout << *iter9 << " ";
+        std::cout << *iter9 << " ";
     }
 }
 
--- src/parse.h
+++ src/parse.h
@@ -2,14 +2,13 @@
 #define __PARSE_H__
 
 #include "common.h"
-// #define GLM_ENABLE_EXPERIMENTAL // FIXME: glm to_string 안쓰게 되면 지우기
-// #include "glm/ext.hpp"          // FIXME: glm to_string 안쓰게 되면 지우기
+#define GLM_ENABLE_EXPERIMENTAL // FIXME: glm to_string 안쓰게 되면 지우기
+#include "glm/ext.hpp"          // FIXME: glm to_string 안쓰게 되면 지우기
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
-
 
 // CLASS_PTR(Parse)
 class Parse
@@ -20,38 +19,40 @@ class Parse
     void makeVBO();
     void parseMtlFile();
     void setFileName(std::string name);
-    void normalizing();
+    void normalizing(std::vector<size_t> facesLine);
 
-  // for debug
+    // for debug
     void printVertexInfo();
     std::string getFileName();
 
   private:
     std::string mtlInfo;
     std::string filename;
-    
+
     // === PARSING =============================
     std::string typePrefix;
     std::size_t vertexNormalIdx;
     std::size_t vertexTexIdx;
     std::size_t rangeLimit;
 
-    // VERTEX data 
-    std::vector<float> vertexPosition; // 3
-    std::vector<float> vertexNormal; // 3
+    // VERTEX data
+    std::vector<std::vector<float>> vertexPosition; // 3
+    std::vector<float> vertexNormal;   // 3
     std::vector<float> vertexTexCoord; // 2
 
     // FACE data
     std::vector<std::vector<size_t>> faces; // 3개의 index가 vector<size_t> 형태로 들어가 있음
 
     // === NORMALIZING =============================
-    std::vector<std::vector<size_t>> normals;
+    std::vector<glm::vec3> normals;
+
 
     // mtl file
     std::vector<float> attribute;
     std::vector<float> diffuse;
     std::vector<float> specular;
-    
+
+    // === GLM =============================
 };
 
 #endif // __PARSE_H__
